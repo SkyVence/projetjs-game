2bc5b9dd97656b1b0910af2e6d1cf07f29a6ddf2
Armand Brethès <armand.brethes@gmail.com>
2026-03-17 17:30:30 +0100

    feat: menu HTML | CSS Prepared

--- src/interface.ts
+++
@@ -1,4 +0,0 @@
-const PlayBtn = document.createElement("button");
-PlayBtn.textContent = "Start Game";
-
-export { PlayBtn };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { PlayBtn } from "@/interface";
+import {
+  ContinueBtn,
+  NewGameBtn,
+  SettingsBtn,
+  ExitBtn,
+  GameTitle,
+  PatchNote,
+} from "@/menu";
 
 const app = document.getElementById("app");
 if (app) {
   app.textContent = "Game loading...";
-  app.appendChild(PlayBtn);
+  app.appendChild(GameTitle);
+  app.appendChild(PatchNote);
+  app.appendChild(ContinueBtn);
+  app.appendChild(NewGameBtn);
+  app.appendChild(SettingsBtn);
+  app.appendChild(ExitBtn);
 }
