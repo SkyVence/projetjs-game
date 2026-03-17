36ff9b95d43b990cf182cdc92189997ea6cf5487
Antoine Mathié <antoine.mathie100@gmail.com>
2026-03-17 15:22:33 +0100

    dom example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.createElement("button");
+  btn.innerText = "This is btn created through the dom";
+  document.body.appendChild(btn);
+  btn.addEventListener("click", function() {
+      console.log("btn clicked");
+  });
+})
