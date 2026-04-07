aaa5e5fb24ecab0015bf37c008f9fc40e5b00d73
Antoine Mathié <antoine.mathie100@gmail.com>
2026-04-07 14:29:45 +0200

    fix build deployed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   root: ".",
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
